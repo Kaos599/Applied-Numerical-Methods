b4a63844a0775462ab34fe24ce28f4235d90fcda
confusedjpeg <128243120+confusedjpeg@users.noreply.github.com>
2023-05-17 00:57:48 +0530

    Update Newton_Backward.py
    Added comments

--- Interpolation Methods/Newton_Backward.py
+++ Interpolation Methods/Newton_Backward.py
@@ -1,26 +1,38 @@
+#IMPORTANT (USAGE INFO)
+
+# Use log fns as log(value, base)
+# Use trignometric fns as cos(x) or sin(x) or tan(x)
+# To use e as e**x use exp(x)
+# To use power use x**y to signify x^y
+# To use multiply use x*y to signify xy
+# To use divide use x/y
+
+
+# import the tabulate module
 from tabulate import tabulate
 
-
+# getting the user input for the number of pairs
 user_input = int(input("Enter number of x and y pairs: "))
 
 def Table_maker(x_values, y_values):
- 
- data1 = {}
+    # creating an empty dictionary to store the data
+    data1 = {}
 
- 
- data1["x"] = x_values
- data1["y"] = y_values
+    # adding the x_values and y_values lists to the data dictionary with keys "x" and "y"
+    data1["x"] = x_values
+    data1["y"] = y_values
 
- 
- print(tabulate(data1, headers="keys", tablefmt="grid"))
+    # printing the data dictionary as a table using tabulate
+    print(tabulate(data1, headers="keys", tablefmt="grid"))
 
 
 
+# creating an empty dictionary to store the data
 data={}
-
+# creating empty list for storing x and y values
 x_values=[]
 y_values=[]
-
+# taking user input for the values of x and y
 for i in range(0,user_input):
  x_value="x{}".format(i)
  data[x_value]=float(input("Enter value of x({}): ".format(i)))
@@ -33,48 +45,47 @@ for i in range(0,user_input):
 Table_maker(x_values, y_values)
 
 def newton_backward(x_values, y_values, x):
- 
- n = len(x_values)
+    # number of data points
+    n = len(x_values)
 
- 
- backward_difference = [[0 for i in range(n)]
- for j in range(n)]
+    # calculating backward difference table
+    backward_difference = [[0 for i in range(n)]
+    for j in range(n)]
 
- 
- for i in range(n):
-    backward_difference[i][0] = y_values[i]
+    # inserting y values in the first column
+    for i in range(n):
+        backward_difference[i][0] = y_values[i]
 
- 
- for i in range(1, n):
-    for j in range(n - 1, i - 1, -1):
-        backward_difference[j][i] = backward_difference[j][i-1] - backward_difference[j-1][i-1]
+    # calculating backward difference table
+    for i in range(1, n):
+        for j in range(n - 1, i - 1, -1):
+            backward_difference[j][i] = backward_difference[j][i-1] - backward_difference[j-1][i-1]
 
- 
- result = backward_difference[n-1][0]
+    # initializing result
+    result = backward_difference[n-1][0]
 
- print("The table is: ")
- for i in range(n - 1, -1, -1):
-    row = []
-    for j in range(n - i):
-        row.append(backward_difference[i][j])
-        print(row)
+    print("The table is: ")
+    for i in range(n - 1, -1, -1):
+        row = []
+        for j in range(n - i):
+            row.append(backward_difference[i][j])
+            print(row)
 
- 
- u = (x - x_values[n-1]) / (x_values[1] - x_values[0])
- for i in range(1, n):
-    term = backward_difference[n-1][i]
-    for j in range(i):
-        term = term * (x - x_values[n-i+j])
-        term = term / (x_values[i] - x_values[j])
-    result = result + term
+    # calculating the value of f(x) using the formula
+    for i in range(1, n):
+        term = backward_difference[n-1][i]
+        for j in range(i):
+            term = term * (x - x_values[n-i+j])
+            term = term / (x_values[i] - x_values[j])
+        result = result + term
 
- return result
+    return result
 
-
+# taking user input for the value of x to be interpolated
 x = float(input("\nEnter the value of x to be interpolated: "))
 
-
+# calculating the value of f(x) using the newton forward interpolation method
 result = newton_backward(x_values, y_values, x)
 
-
+# printing the value of f(x)
 print("\nThe value of f({}) using Newton backward interpolation method is: {}".format(x, result))
