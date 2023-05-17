de1547e3c139173f46dbf4a926b9835a5f065303
confusedjpeg <128243120+confusedjpeg@users.noreply.github.com>
2023-05-18 02:03:06 +0530

    Update Newton Divided Difference.py

--- Interpolation Methods/Newton Divided Difference.py
+++ Interpolation Methods/Newton Divided Difference.py
@@ -1,6 +1,19 @@
+
+#IMPORTANT (USAGE INFO)
+
+# Use log fns as log(value, base)
+# Use trignometric fns as cos(x) or sin(x) or tan(x)
+# To use e as e**x use exp(x)
+# To use power use x**y to signify x^y
+# To use multiply use x*y to signify xy
+# To use divide use x
+
+
+# import the tabulate module
 import numpy as np
 from tabulate import tabulate
 
+#defining function coef(x,y)
 def coef(x, y):
     # Compute the finite divided differences and store them in a
     # one-dimensional array
@@ -11,6 +24,7 @@ def coef(x, y):
             a[j] = (a[j] - a[j-1]) / (x[j] - x[j-i]) # Compute the divided differences
     return a # Return the array of coefficients
 
+#defining function Eval(a,x,r)
 def Eval(a, x, r):
     # Evaluate the polynomial using Horner's method
     n = len(a) - 1
@@ -19,44 +33,50 @@ def Eval(a, x, r):
         p = a[i] + (r - x[i]) * p # Update the polynomial value
     return p # Return the interpolated value
 
+
 def Table_maker(x_values, y_values):
- 
- data1 = {}
+    # creating an empty dictionary to store the data
+    data1 = {}
 
- 
- data1["x"] = x_values
- data1["y"] = y_values
+    # adding the x_values and y_values lists to the data dictionary with keys "x" and "y"
+    data1["x"] = x_values
+    data1["y"] = y_values
 
- 
- print(tabulate(data1, headers="keys", tablefmt="grid"))
+    # printing the data dictionary as a table using tabulate
+    print(tabulate(data1, headers="keys", tablefmt="grid"))
 
 
-
+# creating an empty dictionary to store the data
 data={}
-
+# creating empty list for storing x and y values
 x_values=[]
 y_values=[]
 
+# taking user input for the values of x and y
 user_input = int(input("Enter number of x and y pairs: "))
 
 for i in range(0,user_input):
- x_value="x{}".format(i)
- data[x_value]=float(input("Enter value of x({}): ".format(i)))
- x_values.append(data[x_value])
+    x_value="x{}".format(i)
+    data[x_value]=float(input("Enter value of x({}): ".format(i)))
+    x_values.append(data[x_value])
 for i in range(0,user_input):
- y_value="y{}".format(i)
- data[y_value]=float(input("Enter value of y({}): ".format(i)))
- y_values.append(data[y_value])
+    y_value="y{}".format(i)
+    data[y_value]=float(input("Enter value of y({}): ".format(i)))
+    y_values.append(data[y_value])
 
 Table_maker(x_values, y_values)
 
-a = coef(x_values, y_values) # Compute the coefficients using Newton's divided difference formula
+# Compute the coefficients using Newton's divided difference formula
+a = coef(x_values, y_values) 
 
+# taking user input for the value of x to be interpolated
 x_interpolate = float(input("Enter the value of x to interpolate: "))
 
-y_interpolate = Eval(a, x_values, x_interpolate) # Evaluate the polynomial at the given point
+# Evaluate the polynomial at the given point
+y_interpolate = Eval(a, x_values, x_interpolate) 
 
-
-
-
+# printing the value of f(x)
 print("The interpolated value of y for x = {} is: {}".format(x_interpolate, y_interpolate))
+
+
+
